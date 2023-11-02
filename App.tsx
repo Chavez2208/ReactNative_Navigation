49a3b35365bf7cbae2b78df461cc4dc725bba877
Chavez2208 <iscchavez26@gmail.com>
2023-11-02 16:14:14 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,14 +2,10 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavPersonalizado from './src/navigators/DrawerNavPersonalizado';
-// import { StackNavigator } from './src/navigators/StackNavigator';
-// import { DrawerNavigationMenu } from './src/navigators/DrawerNavigationMenu';
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* <StackNavigator /> */}
-      {/* <DrawerNavigationMenu /> */}
       <DrawerNavPersonalizado />
     </NavigationContainer>
 
