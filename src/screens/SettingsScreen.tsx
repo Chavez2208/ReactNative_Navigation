661f973d8ec3555ef15608a6840096592b7e6286
Chavez2208 <iscchavez26@gmail.com>
2023-11-02 16:39:43 -0600

    Update SettingsScreen.tsx

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text } from 'react-native'
 import { styles } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { AuthContext } from '../context/AuthContext';
 
 const SettingsScreen = () => {
     const insents = useSafeAreaInsets();
+    const { authState, singIn } = useContext(AuthContext);
 
     return (
         <View style={{ ...styles.marginGlobal, marginTop: insents.top }}>
             <Text style={styles.title}>Configuracion General del APP</Text>
+            <Text style={styles.title}>
+                Esto es el estado Global del Context
+
+                {JSON.stringify(authState, null, 3)}
+            </Text>
         </View>
     )
 }
